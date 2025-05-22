1545e3231a5f807d01f7426887a2ca547743b4e8
adinvdas <168001900+adinvdas@users.noreply.github.com>
2025-05-22 13:13:09 +0530

    added a new file

---
+++ day1/greet.js
@@ -0,0 +1,8 @@
+function greetname(name){
+    return `Hello,i am ${name}!`;
+}
+function name1(nm){
+    return `hello my name is ${nm}`;
+}
+console.log(greetname("adin"));
+console.log((name1)("adi"));
