5e719e4f1d3695877bf998c27f087b4d15539c80
adinvdas <168001900+adinvdas@users.noreply.github.com>
2025-05-22 13:34:09 +0530

    added 3 new file file one a bonus one of converting miles into kilometers

---
+++ day1/F_to_C.js
@@ -0,0 +1,6 @@
+// CONVERTING FAHRENHEIT INTO CELSIUS
+function FtoC(F){
+    const celsius =(F - 32)*5/9;
+    return celsius;
+}
+console.log(FtoC(96));
---
+++ day1/mtok.js
@@ -0,0 +1,6 @@
+// code to convert miles into km
+function mtok(m){
+    const km = (m * 1.609344);
+    return km
+}
+console.log(mtok(30));
