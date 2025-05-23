465fe70005745698614a061fb035be53f84077fd
adinvdas <168001900+adinvdas@users.noreply.github.com>
2025-05-23 20:31:56 +0530

    made some few changes using prettier and added a new file

--- day1/greet.js
+++ day1/greet.js
@@ -1,8 +1,8 @@
-function greetname(name){
-    return `Hello,i am ${name}!`;
+function greetname(name) {
+  return `Hello,i am ${name}!`;
 }
-function name1(nm){
-    return `hello my name is ${nm}`;
+function name1(nm) {
+  return `hello my name is ${nm}`;
 }
 console.log(greetname("adin"));
-console.log((name1)("adi"));
+console.log(name1("adi"));
---
+++ day2/iseve.js
@@ -0,0 +1,9 @@
+function iseven(num){
+    return num % 2 === 1;
+}
+let num =13;
+if (iseven(num)){
+    console.log("number is not even")
+}else{
+    console.log("number is even")
+}
