3bf215365ebea32ab8756e51648332e843fa3163
adinvdas <168001900+adinvdas@users.noreply.github.com>
2025-05-24 14:53:38 +0530

    added a new greeting file

---
+++ day2/GM.js
@@ -0,0 +1,19 @@
+function gm(hour){
+    if(hour < 12){
+        return "Good Morning";
+    }else if (hour < 17){
+        return "Good Afternoon";
+    }else if(hour <21){
+        return "Good Evnening";
+    }else if(hour <24){
+        return "Good Night";
+    }else{
+        return "What is this time you dumbo ????";
+    }
+
+}
+console.log(gm(4));
+console.log(gm(14));
+console.log(gm(19));
+console.log(gm(23));
+console.log(gm(25));
